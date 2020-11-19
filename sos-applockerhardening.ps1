70baa12763f631bd3c8fc6ff9d8ca28607a2a360
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-11-19 13:08:04 -0600

    Update sos-applockerhardening.ps1

--- sos-applockerhardening.ps1
+++ sos-applockerhardening.ps1
@@ -9,3 +9,11 @@ do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
 ForEach ($Policy in (Get-ChildItem ./Files/).FullName){
    Set-AppLockerPolicy -XMLPolicy "$Policy" -Merge
 }
+
+# Appplocker service running?
+Set-Service -Name AppIdsvc -StartupType Automatic
+Start-Service AppIdsvc
+Get-Service -Name AppIdsvc | fl St*
+
+#Print Conf
+Get-AppLockerPolicy -Local
